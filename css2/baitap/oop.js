179fe2d803b6c40e5a35c7ab7ad3d933e3af9323
Bùi Thành <k@chupanhsam.com>
2020-10-26 20:56:54 +0700

    chua bai tap css2

--- css2/baitap/oop.js
+++ css2/baitap/oop.js
@@ -3,7 +3,7 @@ class Company {
     establishedDate;//ngay thanh lap
     country;
     businessType;
-    employees;
+    employees = [];
     constructor(nameCompany, establishedDate, country, businessType, employees) {
         this.nameCompany = nameCompany;
         this.establishedDate = establishedDate;
@@ -24,63 +24,45 @@ class Company {
 
     }
 }
-class Employee extends Company {
+class Employee  {
     nameEmplyee;
     sex;
     age;
     address;
-    constructor(nameCompany, establishedDate, country, businessType, employees, nameEmployee, sex, age, address) {
-        super(nameCompany, establishedDate, country, businessType, employees)
+    constructor(nameEmployee, sex, age, address) {
         this.nameEmployee = nameEmployee
         this.sex = sex;
         this.age = age;
         this.address = address
     }
     about() {
-        console.log(`${this.nameEmplyee}, ${this.age}, ${this.address},${this.nameCompany}`)
+        console.log(`${this.nameEmplyee}, ${this.age}, ${this.address}`)
     }
 }
 class Ceo extends Employee {
     strategy;
-    constructor(nameCompany, establishedDate, country, businessType, employees, nameEmployee, sex, age, address, strategy) {
-        super(nameCompany, establishedDate, country, businessType, employees, nameEmployee, sex, age, address)
+    constructor(nameEmployee, sex, age, address, strategy) {
+        super(nameEmployee, sex, age, address)
         this.strategy = strategy
     }
 }
 class Mamager extends Employee {
     managementStyle;
-    constructor(nameCompany, establishedDate, country, businessType, employees, nameEmployee, sex, age, address, managementStyle) {
-        super(nameCompany, establishedDate, country, businessType, employees, nameEmployee, sex, age, address)
+    constructor(nameEmployee, sex, age, address, managementStyle) {
+        super(nameEmployee, sex, age, address)
         this.managementStyle = managementStyle;
-
     }
 }
 
 class Staff extends Employee {
     experience;
-    constructor(nameCompany, establishedDate, country, businessType, employees, nameEmployee, sex, age, address, experience) {
-        super(nameCompany, establishedDate, country, businessType, employees, nameEmployee, sex, age, address)
+    constructor(nameEmployee, sex, age, address, experience) {
+        super(nameEmployee, sex, age, address)
         this.experience = experience
     }
 }
 
-let company = new Company("cong tay 1",20/20/2020,"viet nam","marketing",['nhan vien1','nhan vien 2'])
+let company = new Company("cong tay 1", 20 / 20 / 2020, "viet nam", "marketing", ['nhan vien1', 'nhan vien 2'])
 
-let ceo = new Ceo("cong tay 1",20/20/2020,"viet nam","marketing",['nhan vien1','nhan vien 2'],"ten ceo",'male','24','ha noi','lieu an nhieu')
 
-let managers1 = new Mamager("cong tay 1",20/20/2020,"viet nam","marketing",['nhan vien1','nhan vien 2'],'ten manager','nu',25,'ha noi 2','phong cach quan ly 1')
-let managers1 = new Mamager("cong tay 1",20/20/2020,"viet nam","marketing",['nhan vien1','nhan vien 2'],'ten manager','nu',25,'ha noi 2','phong cach quan ly 1')
-let managers1 = new Mamager("cong tay 1",20/20/2020,"viet nam","marketing",['nhan vien1','nhan vien 2'],'ten manager','nu',25,'ha noi 2','phong cach quan ly 1')
-let managers1 = new Mamager("cong tay 1",20/20/2020,"viet nam","marketing",['nhan vien1','nhan vien 2'],'ten manager','nu',25,'ha noi 2','phong cach quan ly 1')
-let managers1 = new Mamager("cong tay 1",20/20/2020,"viet nam","marketing",['nhan vien1','nhan vien 2'],'ten manager','nu',25,'ha noi 2','phong cach quan ly 1')
-
-let staffs = new Staff("cong tay 1",20/20/2020,"viet nam","marketing",['nhan vien1','nhan vien 2'],"ten nhan vien",'nu',26,'ha noi',2)
-let staffs = new Staff("cong tay 1",20/20/2020,"viet nam","marketing",['nhan vien1','nhan vien 2'],"ten nhan vien",'nu',26,'ha noi',2)
-let staffs = new Staff("cong tay 1",20/20/2020,"viet nam","marketing",['nhan vien1','nhan vien 2'],"ten nhan vien",'nu',26,'ha noi',2)
-let staffs = new Staff("cong tay 1",20/20/2020,"viet nam","marketing",['nhan vien1','nhan vien 2'],"ten nhan vien",'nu',26,'ha noi',2)
-let staffs = new Staff("cong tay 1",20/20/2020,"viet nam","marketing",['nhan vien1','nhan vien 2'],"ten nhan vien",'nu',26,'ha noi',2)
-let staffs = new Staff("cong tay 1",20/20/2020,"viet nam","marketing",['nhan vien1','nhan vien 2'],"ten nhan vien",'nu',26,'ha noi',2)
-let staffs = new Staff("cong tay 1",20/20/2020,"viet nam","marketing",['nhan vien1','nhan vien 2'],"ten nhan vien",'nu',26,'ha noi',2)
-let staffs = new Staff("cong tay 1",20/20/2020,"viet nam","marketing",['nhan vien1','nhan vien 2'],"ten nhan vien",'nu',26,'ha noi',2)
-let staffs = new Staff("cong tay 1",20/20/2020,"viet nam","marketing",['nhan vien1','nhan vien 2'],"ten nhan vien",'nu',26,'ha noi',2)
-let staffs = new Staff("cong tay 1",20/20/2020,"viet nam","marketing",['nhan vien1','nhan vien 2'],"ten nhan vien",'nu',26,'ha noi',2)
+let ceo = new Ceo("")
--- css2/baitap/thuattoan.js
+++ css2/baitap/thuattoan.js
@@ -1,14 +1,37 @@
-let nums = [1, 1, 2, 3, 3, 4, 5, 5]
-let nums2 = [-2,2,4,6,-2,10]
+// let nums = [1, 1, 2, 3, 3, 4, 5, 5]
+// let nums2 = [-2,2,4,6,-2,10]
 
-function khongTrungLap(nums) {
-    nums.sort()
-    for (let index = 0; index < nums.length - 1; index++) {
-        if (nums[index] == nums[index + 1]) {
-            nums.splice(index, 2)
-        }
-    }
-    console.log(nums)
+// function khongTrungLap(nums) {
+//     nums.sort()
+//     for (let index = 0; index < nums.length - 1; index++) {
+//         if (nums[index] == nums[index + 1]) {
+//             nums.splice(index, 2)
+//         }
+//     }
+//     console.log(nums)
+// }
+
+// khongTrungLap(nums2)
+
+let check = {
+
+};
+
+let array = [1,2,1,1,4,3,4,5,3,2]
+
+for (let indelx = 0; indelx < array.length; indelx++) {
+   if(!check[array[indelx]]){
+       check[array[indelx]] = 1;
+   }else{
+       check[array[indelx]]++;
+   } 
 }
 
-khongTrungLap(nums2)
+console.log(check)
+
+for (const key in check) {
+    // console.log(check[key])
+    if(check[key] === 1){
+        console.log(key)
+    }
+}
